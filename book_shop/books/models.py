48a227de486a73939a9b6223de0b790da0378a45
Sergey Karagodin <patridon87@yandex.ru>
2022-06-28 19:07:15 +0300

    models in shop app

--- book_shop/books/models.py
+++ book_shop/books/models.py
@@ -2,36 +2,46 @@ from django.db import models
 
 
 class Author(models.Model):
+    """Модель авторов книг"""
     first_name = models.CharField(max_length=150, verbose_name="Имя автора")
     last_name = models.CharField(max_length=150, verbose_name="Фамилия автора")
     bio = models.TextField(verbose_name="Краткая биография автора")
 
 
 class Category(models.Model):
+    """Модель категорий книг"""
     name = models.CharField(max_length=150, verbose_name="Название категории")
 
 
 class Genre(models.Model):
+    """Модель жанров книг"""
     name = models.CharField(max_length=150, verbose_name="Название жанра")
 
 
 class Series(models.Model):
+    """Модель серий книг"""
     name = models.CharField(max_length=150, verbose_name="Название серии")
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, related_name='series',
+                               on_delete=models.CASCADE)
 
 
 class Book(models.Model):
+    """Модель книг"""
     title = models.CharField(max_length=300, verbose_name="Название книги")
-    author = models.ForeignKey(Author, verbose_name="Автор книги",
+    author = models.ForeignKey(Author, related_name='books',
+                               verbose_name="Автор книги",
                                on_delete=models.CASCADE)
     image = models.ImageField(upload_to="books/",
                               verbose_name="Изображение книги")
-    series = models.ForeignKey(Series, on_delete=models.SET_NULL,
+    series = models.ForeignKey(Series, related_name='books',
+                               on_delete=models.SET_NULL,
                                verbose_name="Серия")
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                               verbose_name="Категория")
-    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL,
-                               verbose_name="Жанр")
+    category = models.ForeignKey(Category, related_name='books',
+                                 on_delete=models.SET_NULL,
+                                 verbose_name="Категория")
+    genre = models.ForeignKey(Genre, related_name='books',
+                              on_delete=models.SET_NULL,
+                              verbose_name="Жанр")
     publishing_year = models.PositiveSmallIntegerField(
         verbose_name="Год публикации книги")
     publishing_house = models.CharField(max_length=150,
--- book_shop/shop/models.py
+++ book_shop/shop/models.py
@@ -1,7 +1,40 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth import get_user_model
+from books.models import Book
 
 
 User = get_user_model()
 
 
+class Cart(models.Model):
+    """Модель для организации корзины"""
+    user = models.ForeignKey(User, related_name="buyer",
+                             verbose_name="Покупатель",
+                             on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name="books",
+                             verbose_name="Книги",
+                             on_delete=models.CASCADE)
+
+
+class Rating(models.Model):
+    """Модель для учета рейтинга книг"""
+    user = models.ForeignKey(User, related_name="buyer",
+                             verbose_name="Покупатель",
+                             on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name="books",
+                             verbose_name="Книги",
+                             on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
+                                                         MaxValueValidator(5)],
+                                             verbose_name="Оценка")
+
+
+class Favorite(models.Model):
+    """Модель для организации списка желаемого"""
+    user = models.ForeignKey(User, related_name="buyer",
+                             verbose_name="Покупатель",
+                             on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name="books",
+                             verbose_name="Книги",
+                             on_delete=models.CASCADE)
